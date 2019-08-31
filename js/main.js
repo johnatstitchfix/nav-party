75378d123607ea43ca4eb6d8e45c9282f2f18278
John Hanawalt <john.hanawalt@stitchfix.com>
2019-08-30 18:09:55 -0700

    Finished first pass at footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(".account-menu__toggle").click(function(){
-  $(".account-menu").toggleClass("open");
+$(".account-menu__toggle").click(function () {
+    $(".account-menu").toggleClass("open");
+});
+$(".footer-toggle").click(function () {
+    $(".footer__main__wrapper").slideToggle();
+    $(".footer__main__wrapper").toggleClass("open");
+    $(".footer-toggle").toggleClass("close")
 });
