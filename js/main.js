2d5ee07dc3aa9a67e7c03ab96d5e6d14e1b3dfcc
John Hanawalt <john.hanawalt@stitchfix.com>
2019-09-03 11:43:48 -0700

    Added nav transition on scroll

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(".footer-toggle").click(function (event) {
     $(".footer-toggle").toggleClass("close")
 });
 
-/**** Moving nav ***/
+/*** Moving nav underscore ***/
 var underscoreWidth = $(".nav--secondary__menu li a.current").width();
 var underscoreLeft = $(".nav--secondary__menu li a.current").position();
 $('#page-underscore').css({"width": underscoreWidth, "left": underscoreLeft.left});
@@ -25,3 +25,43 @@ $(".nav--secondary__menu").mouseleave(
         $('#page-underscore').css({"width": underscoreWidth, "left": underscoreLeft.left});
     }
 );
+
+/*** Nav transition on scroll ***/ 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').addClass('header--up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('header--up');
+        }
+    }
+    
+    lastScrollTop = st;
+}
