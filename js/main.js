ea9a5816c88da41c4b1926187f4658eb15a28131
John Hanawalt <john.hanawalt@stitchfix.com>
2019-09-03 10:42:56 -0700

    Underscore working yay!

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ $(".footer-toggle").click(function (event) {
     $(".footer__main__wrapper").toggleClass("open");
     $(".footer-toggle").toggleClass("close")
 });
+
+/**** Moving nav ***/
+var underscoreWidth = $(".nav--secondary__menu li a.current").width();
+var underscoreLeft = $(".nav--secondary__menu li a.current").position();
+$('#page-underscore').css({"width": underscoreWidth, "left": underscoreLeft.left});
+$(".nav--secondary__menu li a").hover(
+    function () {
+        var newWidth = $(this).width();
+        var newLeft = $(this).position(); 
+        $('#page-underscore').css({"width": newWidth, "left": newLeft.left});
+    }
+);
+$(".nav--secondary__menu").mouseleave(
+    function () {
+        $('#page-underscore').css({"width": underscoreWidth, "left": underscoreLeft.left});
+    }
+);
