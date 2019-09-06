0c3547f823a307781967d5a824a073cd7569fbd9
John Hanawalt <john.hanawalt@stitchfix.com>
2019-09-06 12:25:53 -0700

    Cleaned up code a little bit.
    Mostly just adding notes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-$(".account-menu__toggle").click(function (event) {
-    event.preventDefault();
-    $(".account-menu").toggleClass("open");
-});
-$(document).on("click", function (e) {
-    if ($(e.target).is(".account-menu, .account-menu__toggle, .account-menu__toggle__initial, .account-menu__toggle__name") === false) {
-        $(".account-menu").removeClass("open");
-    }
-});
-$(".nav--primary__menu li a, .nav--secondary__menu li a").on("click", function () {
-    $(this).addClass("current");
-});
-$(".footer-toggle").click(function (event) {
-    event.preventDefault();
-    $(".footer__main__wrapper").slideToggle();
-    $(".footer__main__wrapper").toggleClass("open");
-    $(".footer-toggle").toggleClass("close")
-});
+// NAVIGATION
 $(document).ready(function () {
-    $(".account-menu").css("transition", "all .4s ease"); // Adding this CSS here because otherwise it causes loading jank in Safari
+    $(".account-menu").css("transition", "all .4s ease"); // Adding this CSS here because otherwise it causes loading jank in Safari with CSS Transitions
+
+    /*** Moving highlighting of active navigation ***/
+
     function navHighlighting() {
-        /*** Moving nav dot ***/
+
+        /*** Moving primary nav dot ***/
+
         var dotLeft = $(".nav--primary__menu li a.current").position();
         var menuItemWidth = $(".nav--primary__menu li a.current").width();
         $('#top-level-dot').css({
@@ -41,7 +29,9 @@ $(document).ready(function () {
                 });
             }
         );
-        /*** Moving nav underscore ***/
+
+        /*** Moving secondary nav underscore ***/
+
         var underscoreWidth = $(".nav--secondary__menu li a.current").width();
         var underscoreLeft = $(".nav--secondary__menu li a.current").position();
         $('#page-underscore').css({
@@ -67,12 +57,21 @@ $(document).ready(function () {
             }
         );
     }
+
+    /*** Needs to refire of resize ***/
+
     navHighlighting();
     $(window).resize(navHighlighting);
 });
 
-/*** Nav transition on scroll ***/
-// Hide Header on on scroll down
+/*** Add current class to nav item when clicked to transition the style gently before the page reload ***/
+
+$(".nav--primary__menu li a, .nav--secondary__menu li a").on("click", function () {
+    $(this).addClass("current");
+});
+
+/*** Scrolling to hide/stick header on on scroll ***/
+
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
@@ -109,27 +108,7 @@ function hasScrolled() {
     }
     lastScrollTop = st;
 }
-/*** Tooltips ***/
-$('.style-guide-link').click(function (event) {
-    event.preventDefault();
-    $(".tooltip").toggleClass("active");
-});
-$(document).on("click", function (e) {
-    if ($(e.target).is(".tooltip, .style-guide-link, .style-guide") === false) {
-        $(".tooltip").removeClass("active");
-    }
-});
-/*** Fade out main on link clink ***/
-$('.nav--primary__menu a, .nav--secondary__menu a').click(function (event) {
-    event.preventDefault();
-    newLocation = this.href;
-    $('main').addClass('disappear');
-    setTimeout(newpage, 300);
-});
 
-function newpage() {
-    window.location = newLocation;
-}
 /*** Secondary nav transitions when click on a parent ***/
 $('.nav--primary__menu a').on("click", function () {
     $(".nav--secondary__wrapper").addClass("transition-out");
@@ -140,3 +119,55 @@ $(".nav--primary__menu a.no-sub-nav").on("click", function () {
 $(".nav--primary__menu a:not(.no-sub-nav)").on("click", function () {
     $("body").removeClass("no-sub-nav");
 });
+
+
+// ACCOUNT MENU INTERACTION
+$(".account-menu__toggle").click(function (event) {
+    event.preventDefault();
+    $(".account-menu").toggleClass("open");
+});
+
+/*** Hide menu when you click outside of it ***/
+
+$(document).on("click", function (e) {
+    if ($(e.target).is(".account-menu, .account-menu__toggle, .account-menu__toggle__initial, .account-menu__toggle__name") === false) {
+        $(".account-menu").removeClass("open");
+    }
+});
+
+
+//FOOTER
+
+$(".footer-toggle").click(function (event) {
+    event.preventDefault();
+    $(".footer__main__wrapper").slideToggle();
+    $(".footer__main__wrapper").toggleClass("open");
+    $(".footer-toggle").toggleClass("close")
+});
+
+
+/*** Footer tooltips ***/
+
+$('.style-guide-link').click(function (event) {
+    event.preventDefault();
+    $(".tooltip").toggleClass("active");
+});
+$(document).on("click", function (e) {
+    if ($(e.target).is(".tooltip, .style-guide-link, .style-guide") === false) {
+        $(".tooltip").removeClass("active");
+    }
+});
+
+//PAGE TRANSITION 
+/*** Fade out main on link clink ***/
+
+$('.nav--primary__menu a, .nav--secondary__menu a').click(function (event) {
+    event.preventDefault();
+    newLocation = this.href;
+    $('main').addClass('disappear');
+    setTimeout(newpage, 300);
+});
+
+function newpage() {
+    window.location = newLocation;
+}
